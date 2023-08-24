c7bf44cfb677bd311237abb35a735903c1f8c209
mcappucci34 <m.cappucci34@gmail.com>
2023-08-24 07:12:28 -0400

    9

--- python/9_palindrome_number.py
+++ python/9_palindrome_number.py
@@ -1,11 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or :
+        if x < 0:
             return False
 
         reverse_x = 0
-        while x > reverse_x:
-            reverse_x = reverse_x * 10 + x % 10
-            x //= 10
+        tmp = x
+        while tmp > 0:
+            reverse_x = reverse_x * 10 + tmp % 10
+            tmp //= 10
         
-        return x == reverse_x or x == reverse_x // 10
+        return x == reverse_x
