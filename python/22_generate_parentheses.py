96a933ccd9e1a5286f657fb0f415c5f8f33decca
mcappucci34 <m.cappucci34@gmail.com>
2023-09-26 05:59:06 -0400

    22

--- python/22_generate_parentheses.py
+++ python/22_generate_parentheses.py
@@ -1,19 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
 
-        def recGenerate(k):
-            if k == 1:
-                return ['()']
-            result = set()
-            for i in range(1, k):
-                left = recGenerate(i)
-                right = recGenerate(k-i)
-                for l in left:
-                    for r in right:
-                        result.add(l + r)
-                    if i == k-1:
-                        result.add('(' + l + ')')
+        def recGenerate(s, left_count, right_count):
+            if left_count == n and right_count == n:
+                return [s]
+            result = []
+            if left_count < n:
+                result += recGenerate(s + '(', left_count + 1, right_count)
+            if right_count < left_count:
+                result += recGenerate(s + ')', left_count, right_count + 1)
             return result
-
             
-        return recGenerate(n)
+        return recGenerate('', 0, 0)
