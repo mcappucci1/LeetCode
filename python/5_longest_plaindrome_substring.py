33f8a059c221ae29a80f47f4ba8a178ba203e609
mcappucci34 <m.cappucci34@gmail.com>
2023-10-28 08:37:31 -0400

    5

--- python/5_longest_plaindrome_substring.py
+++ python/5_longest_plaindrome_substring.py
@@ -2,19 +2,18 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
 
         n = len(s)
+        longest = (0,0)
 
-        def findLongestPalindrome(left, right):
-            while left >= 0 and right < n and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return s[left + 1: right]
-        
-        longest = ""
+        def palLength(l, r):
+            nonlocal longest
+            while l > -1 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            if (r - l - 2) > (longest[1] - longest[0]):
+                longest = (l+1, r-1)
 
-        for i in range(n):
-            one = findLongestPalindrome(i, i)
-            two = findLongestPalindrome(i, i + 1)
-            longest = longest if len(longest) > len(one) else one
-            longest = longest if len(longest) > len(two) else two
+        for i in range(n-1):
+            palLength(i, i)
+            palLength(i, i+1)
 
-        return longest
+        return s[longest[0]:longest[1]+1]
