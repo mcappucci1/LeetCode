5221247ece3059851a5619e13ac180a972178f19
mcappucci34 <m.cappucci34@gmail.com>
2023-11-07 20:12:47 -0500

    71

---
+++ python/71_simplify_path.py
@@ -0,0 +1,11 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        path = path.split('/')
+        for elem in path:
+            if elem == '..':
+                if stack:
+                    stack.pop()
+            elif elem != '.' and elem != '':
+                stack.append(elem)
+        return '/' + '/'.join(stack)
