8d43d416745e0462da8ca7eb86eb1232e7cb5cfa
mcappucci34 <m.cappucci34@gmail.com>
2023-11-23 10:03:31 -0500

    402

--- python/402_remove_k_digits.py
+++ python/402_remove_k_digits.py
@@ -1,23 +1,10 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         stack = []
-        i = 0
-        while i < len(num):
-            if stack and int(stack[-1]) > int(num[i]):
+        for digit in num:
+            while k and stack and stack[-1] > digit:
                 stack.pop()
                 k -= 1
-                if k == 0:
-                    break
-                i -= 1
-            else:
-                stack.append(num[i])
-            i += 1
-        while k > 0 and stack:
-            stack.pop()
-            k -= 1
-        s = ''.join(stack) + num[i:]
-        i = 0
-        while i < len(s)-1 and s[i] == '0':
-            i += 1
-        s = s[i:]
-        return s or '0'
+            stack.append(digit)
+        stack = stack if k == 0 else stack[:-k]
+        return ''.join(stack).lstrip('0') or '0'
