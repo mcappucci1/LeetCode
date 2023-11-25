fb1357a93c23e506b414646323708d94772c95f6
mcappucci34 <m.cappucci34@gmail.com>
2023-11-25 13:19:27 -0500

    2408

---
+++ python/2408_design_SQL.py
@@ -0,0 +1,20 @@
+class SQL:
+
+    def __init__(self, names: List[str], columns: List[int]):
+        self.tables = defaultdict(list)
+
+    def insertRow(self, name: str, row: List[str]) -> None:
+        self.tables[name].append(row)
+
+    def deleteRow(self, name: str, rowId: int) -> None:
+        self.tables[name][rowId-1] = None
+
+    def selectCell(self, name: str, rowId: int, columnId: int) -> str:
+        return self.tables[name][rowId-1][columnId-1]
+
+
+# Your SQL object will be instantiated and called as such:
+# obj = SQL(names, columns)
+# obj.insertRow(name,row)
+# obj.deleteRow(name,rowId)
+# param_3 = obj.selectCell(name,rowId,columnId)
