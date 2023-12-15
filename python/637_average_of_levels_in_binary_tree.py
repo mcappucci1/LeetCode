c987ad23735e2452511534d85f2c8b027b6a323b
mcappucci34 <m.cappucci34@gmail.com>
2023-12-15 15:55:44 -0500

    637

---
+++ python/637_average_of_levels_in_binary_tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        result = []
+        q = deque([root])
+        while q:
+            level_size = len(q)
+            total = 0
+            for i in range(level_size):
+                node = q.popleft()
+                total += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            result.append(total / level_size)
+        return result
