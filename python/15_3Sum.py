cb0cbf8d367eac9beca89180b40adb60ea86dae1
mcappucci34 <m.cappucci34@gmail.com>
2024-01-07 15:31:48 -0500

    15

---
+++ python/15_3Sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        nums.sort()
+        
+        n = len(nums)
+        triples = []
+        
+        for i in range(n-2):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i] != nums[i-1]:
+                l, r = i+1, n-1
+                while l < r:
+                    total = nums[i] + nums[l] + nums[r]
+                    if total == 0:
+                        triples.append([nums[i], nums[l], nums[r]])
+                    if total <= 0:
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    else:
+                        r -= 1
+                        while l < r and nums[r] == nums[r+1]:
+                            r -= 1
+
+        return triples
