c9b2c70da2f22b50fad5e6d61b397037cb8c72fd
mcappucci34 <m.cappucci34@gmail.com>
2024-01-22 18:53:42 -0500

    645

---
+++ python/645_set_mismatch.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            while nums[i] != i+1 and nums[i] != nums[nums[i]-1]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+        for i in range(len(nums)):
+            if i+1 != nums[i]:
+                return [nums[i], i+1]
