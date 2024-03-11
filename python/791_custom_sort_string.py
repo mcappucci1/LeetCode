d3c6734c9392de62b1160f434876b52d21237758
mcappucci34 <m.cappucci34@gmail.com>
2024-03-11 19:44:25 -0400

    781

--- python/791_custom_sort_string.py
+++ python/791_custom_sort_string.py
@@ -1,18 +1,3 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        
-        counts = [0] * 26
-        norm = ord('a')
-        for c in s:
-            counts[ord(c) - norm] += 1
-        
-        result = []
-        for c in order:
-            if counts[ord(c) - norm] > 0:
-                result.extend([c] * counts[ord(c) - norm])
-                counts[ord(c) - norm] = 0
-        for i in range(26):
-            if counts[i] > 0:
-                result.extend([chr(i+norm)] * counts[i])
-        
-        return ''.join(result)
+        return ''.join(sorted(s, key=lambda c: order.index(c) if c in order else inf))
