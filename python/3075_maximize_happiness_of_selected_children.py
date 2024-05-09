b2faf78892ac4efb668ee5615d89ecb9e2e78306
mcappucci34 <m.cappucci34@gmail.com>
2024-05-09 06:43:51 -0400

    3075

---
+++ python/3075_maximize_happiness_of_selected_children.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+
+        happiness.sort(reverse=True)
+        total = 0
+
+        for i in range(k):
+            if happiness[i] - i <= 0:
+                return total
+            total += happiness[i] - i
+
+        return total
